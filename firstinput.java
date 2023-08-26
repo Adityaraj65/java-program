97845ded7f28628470620b5499ff447a50a5a4d2
aditya raj <116576092+Adityaraj65@users.noreply.github.com>
2023-08-26 16:56:59 +0530

    Add files via upload

---
+++ firstinput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+/**
+ * firstinput
+ */
+public class firstinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+        float price = sc.nextFloat();
+        System.out.println(price);
+        sc.close();
+    
+    }
+}
---
+++ practice.java
@@ -0,0 +1,14 @@
+/**
+ * practice
+ */
+public class practice {
+
+    public static void main(String[] args) {
+        int counter  = 0;
+        while(counter <10){
+            System.out.println("hello world");
+            counter++;
+        }
+        System.out.println("printed hello world 10 times");
+    }
+}
